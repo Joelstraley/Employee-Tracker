77d7a1bf46dfbe04299597cf6c38119ed7aea970
Joel Straley <66093135+Joelstraley@users.noreply.github.com>
2021-02-09 23:49:52 -0500

    Delete Untitled.sql

--- Untitled.sql
+++
@@ -1,4 +0,0 @@
-SELECT employee.id, employee.first_name, employee.last_name, role.title
-FROM employee LEFT JOIN role on employee.role_id = role.id 
-WHERE employee.manager_id = 3
-AND employee.id != employee.manager_id; 
